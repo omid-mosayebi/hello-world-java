0aaeace35b7bc54d8082f3e93755b6fcf3fbfd28
omid-mosayebi <165131974+omid-mosayebi@users.noreply.github.com>
2025-11-13 16:52:03 -0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello omid, World!");
+        System.out.println("Hello omid In Vancouver, World!");
     }
 }
